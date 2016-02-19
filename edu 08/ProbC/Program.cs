9dd383eaac28157cc962bfba14ee049dd9eb8747
fcxxzux <fcxxzux@qq.com>
2016-02-20 02:28:33 +0800

    Round #8
    Round #8 in C# Added

---
+++ edu 08/ProbC/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Numerics;
+using E = System.Linq.Enumerable;
+
+namespace ProbC {
+    class Program {
+        protected IOHelper io;
+
+        public Program(string inputFile, string outputFile) {
+            io = new IOHelper(inputFile, outputFile, Encoding.Default);
+
+            int n = io.NextInt(), k = io.NextInt();
+            string s = io.NextToken();
+            StringBuilder s2 = new StringBuilder("");
+            for (int i = 0; i < s.Length; i++) {
+                if (k <= 0) s2.Append(s[i]);
+                else {
+                    int maxdis;
+                    if (s[i] <= 'm') {
+                        maxdis = 'z' - s[i];
+                        if (maxdis <= k) {
+                            s2.Append('z');
+                            k -= maxdis;
+                        } else {
+                            char newchar = (char)(s[i] + k);
+                            s2.Append(newchar);
+                            k = 0;
+                        }
+                    } else {
+                        maxdis = s[i]-'a';
+                        if (maxdis <= k) {
+                            s2.Append('a');
+                            k -= maxdis;
+                        } else {
+                            char newchar = (char)(s[i] - k);
+                            s2.Append(newchar);
+                            k = 0;
+                        }
+                    }
+                }
+            }
+            io.WriteLine(k > 0 ? "-1" : s2.ToString());
+            io.Dispose();
+        }
+
+        static void Main(string[] args) {
+            Program myProgram = new Program(null, null);
+        }
+    }
+
+    class IOHelper : IDisposable {
+        public StreamReader reader;
+        public StreamWriter writer;
+
+        public IOHelper(string inputFile, string outputFile, Encoding encoding) {
+            if (inputFile == null)
+                reader = new StreamReader(Console.OpenStandardInput(), encoding);
+            else
+                reader = new StreamReader(inputFile, encoding);
+
+            if (outputFile == null)
+                writer = new StreamWriter(Console.OpenStandardOutput(), encoding);
+            else
+                writer = new StreamWriter(outputFile, false, encoding);
+
+            curLine = new string[] { };
+            curTokenIdx = 0;
+        }
+
+        string[] curLine;
+        int curTokenIdx;
+
+        char[] whiteSpaces = new char[] { ' ', '\t', '\r', '\n' };
+
+        public bool hasNext() {
+            if (curTokenIdx >= curLine.Length) {
+                //Read next line
+                string line = reader.ReadLine();
+                if (line != null)
+                    curLine = line.Split(whiteSpaces, StringSplitOptions.RemoveEmptyEntries);
+                else
+                    curLine = new string[] { };
+                curTokenIdx = 0;
+            }
+
+            return curTokenIdx < curLine.Length;
+        }
+
+        public string NextToken() {
+            return hasNext() ? curLine[curTokenIdx++] : null;
+        }
+
+        public int NextInt() {
+            return int.Parse(NextToken());
+        }
+
+        public double NextDouble() {
+            string tkn = NextToken();
+            return double.Parse(tkn, System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        public void Write(double val, int precision) {
+            writer.Write(val.ToString("F" + precision, System.Globalization.CultureInfo.InvariantCulture));
+        }
+
+        public void Write(object stringToWrite) {
+            writer.Write(stringToWrite);
+        }
+
+        public void WriteLine(double val, int precision) {
+            writer.WriteLine(val.ToString("F" + precision, System.Globalization.CultureInfo.InvariantCulture));
+        }
+
+        public void WriteLine(object stringToWrite) {
+            writer.WriteLine(stringToWrite);
+        }
+
+        public void Dispose() {
+            try {
+                if (reader != null) {
+                    reader.Dispose();
+                }
+                if (writer != null) {
+                    writer.Flush();
+                    writer.Dispose();
+                }
+            } catch { };
+        }
+
+
+        public void Flush() {
+            if (writer != null) {
+                writer.Flush();
+            }
+        }
+    }
+}
